4b4220297a4a102e0bdb36ff2980a4617bb955d8
root <root@ip-172-31-35-150.eu-central-1.compute.internal>
2018-10-07 11:41:03 +0000

    build3

--- addressbook_main/src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
+++ addressbook_main/src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
@@ -17,7 +17,7 @@ public class ContactService {
 
     // Create dummy data by randomly combining first and last names
     static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
-            "Nina", "Alex", "Ram", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
+            "Nina", "Alex", "Ram", "shyam", "Umberto", "Henrik", "Rene", "Lisa",
             "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
             "Jennifer" };
     static String[] lnames = { "Smith", "Johnson", "Williams", "Jones",
